be96740d42950f3520b3ef7b4dff4a25aa60740f
Natalia <natali.efimova.2000@gmail.com>
2020-10-17 18:25:54 +0300

    Добавьте файлы проекта.

---
+++ password ex 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password_ex_1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var rnd = new Random();
+            string A = "0123456789ABCDEFGHIGKLMNOPQISTUVWXYZ";
+            Console.WriteLine("Сколько паролей нужно создать?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            for ( int k=0; k <= n; k++)
+            {
+                string passwd = "";
+                for (int i = 0; i < 8; i++)
+                {
+                    passwd += A[rnd.Next(0, 36)];
+                }
+                Console.WriteLine(passwd);
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
